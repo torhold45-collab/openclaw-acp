ac1d4e2d28a31051002aa508ef338a43afa82b18
torhold45-collab <torhold45@gmail.com>
2026-04-13 20:06:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,7 @@ from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 from web3 import Web3
-from virtuals_sdk.game_objects.agent import Agent
-from virtuals_sdk.game_objects.wallet import Wallet
+import virtuals_sdk # Импортируем всю библиотеку целиком
 from dotenv import load_dotenv
 
 load_dotenv()
